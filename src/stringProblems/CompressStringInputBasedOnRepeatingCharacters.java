26ee21b63dd5fc8019596bccb9ae59a63f57a1fb
Zankhana1995 <zankhanapatel1995.zp@gmail.com>
2025-07-09 19:06:49 -0400

    Compress String Input Based On Repeating Characters

---
+++ src/stringProblems/CompressStringInputBasedOnRepeatingCharacters.java
@@ -0,0 +1,33 @@
+package stringProblems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CompressStringInputBasedOnRepeatingCharacters {
+    public static void main(String[] args) {
+        // Compress String inputstr = "aaabb3sdaab"; outputstr = "a3b23sda2b";
+        String input = "aaabb3sdaab";
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        for (int i = 1; i < input.length() ; i++) {
+            if (input.charAt(i-1) == input.charAt(i)) {
+                count ++;
+            } else {
+                if (count == 1) {
+                    sb.append(input.charAt(i - 1));
+                } else {
+                    sb.append(input.charAt(i - 1)).append(count);
+                }
+                count = 1;
+            }
+        }
+
+        // Append the last group
+        if (count == 1) {
+            sb.append(input.charAt(input.length() - 1));
+        } else {
+            sb.append(input.charAt(input.length() - 1)).append(count);
+        }
+        System.out.println(sb);
+    }
+}
