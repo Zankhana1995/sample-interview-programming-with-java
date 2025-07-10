fcc9793d6c69b58ae38593a2f0a0bf8020e59a0f
Zankhana1995 <zankhanapatel1995.zp@gmail.com>
2025-07-10 00:50:48 -0400

    Refactor FirstUniqueElement to use collection

--- src/arrayProblems/FirstUniqueElement.java
+++ src/arrayProblems/FirstUniqueElement.java
@@ -1,8 +1,12 @@
 package arrayProblems;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class FirstUniqueElement {
     public static void main(String[] args) {
         // mastercard round 2
+        // Result the first Element which doesn't repeat in the array
         /**
          arr = {1, 4, 5, 1, 2, 0, 4, 5};
          res = 2
@@ -28,8 +32,10 @@ public class FirstUniqueElement {
         }
         System.out.println(res);
         anotherWay(arr);
+        usingCollections(arr);
     }
 
+    // O(n^2) complexity
     private static void anotherWay(int[] arr) {
         int result = 0;
         for (int i =0 ; i< arr.length ; i++) {
@@ -49,9 +55,23 @@ public class FirstUniqueElement {
             }
         }
     }
+
+    // ----------------- Another way : Using Collection : O(n) complexity -----------------//
+    private static void usingCollections(int[] arr) {
+        Map<Integer, Integer> freqMap = new LinkedHashMap<>();
+        //  {1, 4, 5, 1, 2, 0, 4, 5};
+        for(int n : arr) {
+            freqMap.put(n, freqMap.getOrDefault(n, 0) + 1);
+        }
+        int result = 0 ;
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            if(entry.getValue() == 1) {
+                result = entry.getKey();
+                break;
+            }
+        }
+        System.out.println("Using collection result => " + result);
+    }
 }
-
-// ----------------- Another way -----------------//
-
 
 
