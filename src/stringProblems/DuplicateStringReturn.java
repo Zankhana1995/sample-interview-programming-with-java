0a93f0ddd3074b007c833020eab7dfd2a1841565
Zankhana1995 <zankhanapatel1995.zp@gmail.com>
2025-08-18 18:26:28 -0400

    Duplicate String return, a new method added with less complexity

--- src/stringProblems/DuplicateStringReturn.java
+++ src/stringProblems/DuplicateStringReturn.java
@@ -1,17 +1,28 @@
 package stringProblems;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class DuplicateStringReturn {
     public static void main(String[] args) {
         String[] array = {"apple", "banana", "grapes", "orange", "banana", "kiwi"};
         String s = duplicateReturn(array);
         String res = duplicateReturnWithCollection(array);
+        String result = usingSorting(array);
         System.out.println(s);
         System.out.println(res);
+        System.out.println(result);
+    }
+
+    //easiest way and less complexity -> n log(n)
+    private static String usingSorting(String[] array) {
+        Arrays.sort(array);
+        System.out.println("Sorted array "+ Arrays.toString(array));
+        for (int i=0; i < array.length -1 ; i++) {
+            if (Objects.equals(array[i + 1], array[i])) {
+                return array[i+1];
+            }
+        }
+        return "";
     }
 
     private static String duplicateReturnWithCollection(String[] array) {
