9b908a65b442e1953fd3106a81ecfdf7260a9616
Zankhana1995 <zankhanapatel1995.zp@gmail.com>
2026-01-28 15:23:17 -0500

    Added RemoveDuplicateFromArrayUsingCollection, added converting array to set, list (to/from)

---
+++ src/arrayProblems/RemoveDuplicateFromArrayUsingCollection.java
@@ -0,0 +1,25 @@
+package arrayProblems;
+
+import java.util.*;
+
+public class RemoveDuplicateFromArrayUsingCollection {
+    public static void main(String[] args) {
+        // Example array
+        String[] array = {"apple", "banana", "apple", "orange", "banana"};
+
+        // Convert Array to List
+        List<String> stringList = new ArrayList<>(Arrays.asList(array));
+
+        // Convert List to Set, LinkedHashSet to maintain insertion order
+        Set<String> set = new LinkedHashSet<>(stringList);
+        // Or directly convert Array to set.
+        // Set<String> setTwo = new LinkedHashSet<>(Arrays.asList(array));
+
+        // Convert back to array (Just for knowledge, not needed here)
+        String[] finalResult = set.toArray(new String[0]);
+
+        System.out.println(Arrays.toString(finalResult));
+
+
+    }
+}
